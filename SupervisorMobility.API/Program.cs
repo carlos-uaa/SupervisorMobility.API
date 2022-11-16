ea3f749bbcd62548ca535fe9ce185187fe61dbe9
Juan Herrera (Teddy) <juan.herreraespinoza@compas-mx.com>
2022-11-16 15:34:44 -0600

    add missing settings for publishing.

--- SupervisorMobility.API/Program.cs
+++ SupervisorMobility.API/Program.cs
@@ -38,7 +38,7 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
