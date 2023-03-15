6f84dd8a2f1cb44a7f6ad939203278257d2f0c25
Marco Antonio Aguayo <MikoGrenore@Outlook.com>
2023-03-15 13:44:29 -0600

    Use New Ports

--- SupervisorMobility.API/Migrations/SupervisorMobilityContextModelSnapshot.cs
+++ SupervisorMobility.API/Migrations/SupervisorMobilityContextModelSnapshot.cs
@@ -336,7 +336,7 @@ namespace SupervisorMobility.API.Migrations
                             IsAdmin = true,
                             IsOperator = false,
                             IsSupervisor = true,
-                            LastUpdated = new DateTime(2023, 3, 10, 12, 44, 9, 201, DateTimeKind.Local).AddTicks(2381),
+                            LastUpdated = new DateTime(2023, 3, 14, 16, 41, 7, 985, DateTimeKind.Local).AddTicks(7392),
                             Name = "Marco Aguayo",
                             Payroll = 239935,
                             PlantId = 1
@@ -679,13 +679,7 @@ namespace SupervisorMobility.API.Migrations
                     b.Property<int?>("AreaId")
                         .HasColumnType("int");
 
-                    b.Property<string>("CArea")
-                        .HasColumnType("nvarchar(max)");
-
                     b.Property<string>("Cicles")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("DArea")
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<DateTime?>("DateEnd")
@@ -729,17 +723,8 @@ namespace SupervisorMobility.API.Migrations
                     b.Property<int?>("Option")
                         .HasColumnType("int");
 
-                    b.Property<string>("OthersArea")
-                        .HasColumnType("nvarchar(max)");
-
                     b.Property<int?>("PlantId")
                         .HasColumnType("int");
-
-                    b.Property<string>("QArea")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("SArea")
-                        .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("SsvCommentary")
                         .HasColumnType("nvarchar(max)");
@@ -773,12 +758,10 @@ namespace SupervisorMobility.API.Migrations
                         {
                             JobObservationId = 1,
                             AreaId = 1,
-                            CArea = "Lorem ipsum dolor sit amet C Area",
                             Cicles = "1 min|2 min|3 min|4 min| 5 min",
-                            DArea = "Lorem ipsum dolor sit amet D Area",
-                            DateEnd = new DateTime(2023, 3, 10, 12, 44, 9, 201, DateTimeKind.Local).AddTicks(1123),
-                            DateFinalized = new DateTime(2023, 3, 10, 12, 44, 9, 201, DateTimeKind.Local).AddTicks(1124),
-                            DateStart = new DateTime(2023, 3, 10, 12, 44, 9, 201, DateTimeKind.Local).AddTicks(1005),
+                            DateEnd = new DateTime(2023, 3, 14, 16, 41, 7, 985, DateTimeKind.Local).AddTicks(7039),
+                            DateFinalized = new DateTime(2023, 3, 14, 16, 41, 7, 985, DateTimeKind.Local).AddTicks(7041),
+                            DateStart = new DateTime(2023, 3, 14, 16, 41, 7, 985, DateTimeKind.Local).AddTicks(7008),
                             DistributionId = 1,
                             IsActive = true,
                             Models = "P71A|X247|P71A|X247|P71A",
@@ -788,10 +771,7 @@ namespace SupervisorMobility.API.Migrations
                             OperatorCommentary = "Operator Commentary",
                             OperatorSignature = "Juan",
                             Option = 1,
-                            OthersArea = "Lorem ipsum dolor sit amet Others Area",
                             PlantId = 1,
-                            QArea = "Lorem ipsum dolor sit amet Q Area",
-                            SArea = "Lorem ipsum dolor sit amet S Area",
                             SsvCommentary = "Senior Supervisor Commentary",
                             SsvSignature = "Pedro",
                             Status = 0,
@@ -949,8 +929,8 @@ namespace SupervisorMobility.API.Migrations
                         new
                         {
                             LupId = 1,
-                            CreatedDate = new DateTime(2023, 3, 10, 12, 44, 9, 201, DateTimeKind.Local).AddTicks(2433),
-                            EndDate = new DateTime(2023, 3, 10, 12, 44, 9, 201, DateTimeKind.Local).AddTicks(2435),
+                            CreatedDate = new DateTime(2023, 3, 14, 16, 41, 7, 985, DateTimeKind.Local).AddTicks(7408),
+                            EndDate = new DateTime(2023, 3, 14, 16, 41, 7, 985, DateTimeKind.Local).AddTicks(7410),
                             IsActive = true,
                             JobObservationId = 1,
                             Observer = "Pedro",
