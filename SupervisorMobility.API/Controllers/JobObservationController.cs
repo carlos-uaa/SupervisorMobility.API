0266e602e2e091220f5cae78af5d1d670b2374eb
Marco Antonio Aguayo <MikoGrenore@Outlook.com>
2023-04-13 10:55:41 -0600

    .

--- SupervisorMobility.API/Controllers/JobObservationController.cs
+++ SupervisorMobility.API/Controllers/JobObservationController.cs
@@ -38,7 +38,7 @@ namespace SupervisorMobility.API.Controllers
             _mapper = mapper ??
                 throw new ArgumentNullException(nameof(mapper));
             _assyChartService = assyChartService;
-             
+
         }
 
         [HttpPost]
@@ -89,7 +89,7 @@ namespace SupervisorMobility.API.Controllers
         public async Task<ActionResult<IEnumerable<JobObservationHistoryDto>>> GetHistoryJobObservationsAsync(int jobObservationId)
         {
             var allHistory = await _supervisorMobilityRepository.GetAllHistoryJobObservationAsync(jobObservationId);
-           
+
             return Ok(_mapper.Map<IEnumerable<JobObservationHistoryDto>>(allHistory));
         }
 
@@ -144,7 +144,6 @@ namespace SupervisorMobility.API.Controllers
         }
 
 
-
         [HttpPut("{jobObservationId}")]
         public async Task<ActionResult> UpdateJobObservation(int jobObservationId, RequestJobObservationADuser request)
         {
@@ -180,7 +179,7 @@ namespace SupervisorMobility.API.Controllers
                 return NotFound("Job Observation Not Found");
             }
 
-            if(jobObservationForUpdate.Status == 6 && jobObservationEntity.Status != jobObservationForUpdate.Status)
+            if (jobObservationForUpdate.Status == 6 && jobObservationEntity.Status != jobObservationForUpdate.Status && auser.name != "S.M. System")
             {
                 //crear notificacion
                 NotificationToCreateDto newnotify = new NotificationToCreateDto();
@@ -192,139 +191,141 @@ namespace SupervisorMobility.API.Controllers
                 newnotify.NotificationType = "FinishJobObservation";
 
                 var notadd = await _assyChartService.CreateNotificationAsync(newnotify);
-                if(notadd != null)
+                if (notadd != null)
                 {
                     var emailMessage = _email.CreateEmailMessage(auser.email, "Este es un mensaje de prueba enviado desde job observation");
                     _email.Send(emailMessage);
                 }
-            } 
-
-            //Crear copia de la version anterior
-            JobObservationVersion HistoryToAdd = await _assyChartService.CreateHistoryJobObservationAsync(jobObservationEntity);
-
-            //Creamos mensaje de cambios
-            string resumeChanges = "";
-
-            if (jobObservationEntity.IsActive != jobObservationForUpdate.IsActive)
-            {
-                resumeChanges += "IsActive, ";
-            }
-            if (jobObservationEntity.PlantId != jobObservationForUpdate.PlantId)
-            {
-                resumeChanges += "plant, ";
-            }
-            if (jobObservationEntity.AreaId != jobObservationForUpdate.AreaId)
-            {
-                resumeChanges += "area, ";
-            }
-            if (jobObservationEntity.DistributionId != jobObservationForUpdate.DistributionId)
-            {
-                resumeChanges += "distribution, ";
-            }
-            if (jobObservationEntity.OperationId != jobObservationForUpdate.OperationId)
-            {
-                resumeChanges += "operation, ";
-            }
-            if (jobObservationEntity.SupervisorId != jobObservationForUpdate.SupervisorId)
-            {
-                resumeChanges += "supervisor, ";
-            }
-            if (jobObservationEntity.OperatorId != jobObservationForUpdate.OperatorId)
-            {
-                resumeChanges += "operator, ";
-            }
-            if (jobObservationEntity.Type != jobObservationForUpdate.Type)
-            {
-                resumeChanges += "Type, ";
-            }
-            if (jobObservationEntity.StartDate != jobObservationForUpdate.StartDate)
-            {
-                resumeChanges += "date Start, ";
-            }
-            if (jobObservationEntity.EndDate != jobObservationForUpdate.EndDate)
-            {
-                resumeChanges += "date End, ";
-            }
-            if (jobObservationEntity.FinishedDate != jobObservationForUpdate.FinishedDate)
-            {
-                resumeChanges += "Date Finalized, ";
-            }
-            if (jobObservationEntity.Justification != jobObservationForUpdate.Justification)
-            {
-                resumeChanges += "Justification , ";
-            }
-            if (jobObservationEntity.Status != jobObservationForUpdate.Status)
-            {
-                resumeChanges += "Status, ";
-            }
-            if (jobObservationEntity.Option != jobObservationForUpdate.Option)
-            {
-                resumeChanges += "Option, ";
-            }
-            if (jobObservationEntity.Anomaly != jobObservationForUpdate.Anomaly)
-            {
-                resumeChanges += "Anomaly, ";
-            }
-            if (jobObservationEntity.Time1HOE != jobObservationForUpdate.Time1HOE)
-            {
-                resumeChanges += "Timers 1, ";
-            }
-            if (jobObservationEntity.Time2HOE != jobObservationForUpdate.Time2HOE)
-            {
-                resumeChanges += "Timers 2, ";
-            }
-            if (jobObservationEntity.Models != jobObservationForUpdate.Models)
-            {
-                resumeChanges += "Models, ";
-            }
-            if (jobObservationEntity.Cicles != jobObservationForUpdate.Cicles)
-            {
-                resumeChanges += "Cicles, ";
-            }
-            if (jobObservationEntity.SsvCommentary != jobObservationForUpdate.SsvCommentary)
-            {
-                resumeChanges += "SsvCommentary, ";
-            }
-            if (jobObservationEntity.OperatorCommentary != jobObservationForUpdate.OperatorCommentary)
-            {
-                resumeChanges += "OperatorCommentary, ";
-            }
-            if (jobObservationEntity.SsvSignature != jobObservationForUpdate.SsvSignature)
-            {
-                resumeChanges += "SsvSignature, ";
-            }
-            if (jobObservationEntity.OperatorSignature != jobObservationForUpdate.OperatorSignature)
-            {
-                resumeChanges += "OperatorSignature, ";
             }
 
-            // Remove the trailing comma and space
-            if (resumeChanges.EndsWith(", "))
-            {
-                resumeChanges = resumeChanges.Substring(0, resumeChanges.Length - 2);
-            }
+           
+                //Crear copia de la version anterior
+                JobObservationVersion HistoryToAdd = await _assyChartService.CreateHistoryJobObservationAsync(jobObservationEntity);
 
-            //Actualiza la jobobsevation
-            _mapper.Map(jobObservationForUpdate, jobObservationEntity);
+                //Creamos mensaje de cambios
+                string resumeChanges = "";
 
-            //añadimos la version anterior a la jobOb actualizada
-            if (HistoryToAdd != null)
-            {
-                //optenemos la nueva version
-                var jobtoaddversion = await _supervisorMobilityRepository.GetJobObservationAsync(jobObservationId, true);
-                HistoryToAdd.DateModification = DateTime.Now;
-                HistoryToAdd.resumeVersion = resumeChanges;
-                HistoryToAdd.MadeBy = auser.name;
-                //añadimos
-                bool added = await _supervisorMobilityRepository.AddHistoyToJobObservationAsync(HistoryToAdd, jobtoaddversion);
-                //add to 
-
-                if (!added)
+                if (jobObservationEntity.IsActive != jobObservationForUpdate.IsActive)
                 {
-                    return NotFound("Fail updating history");
+                    resumeChanges += "IsActive, ";
                 }
-                
-            }
+                if (jobObservationEntity.PlantId != jobObservationForUpdate.PlantId)
+                {
+                    resumeChanges += "plant, ";
+                }
+                if (jobObservationEntity.AreaId != jobObservationForUpdate.AreaId)
+                {
+                    resumeChanges += "area, ";
+                }
+                if (jobObservationEntity.DistributionId != jobObservationForUpdate.DistributionId)
+                {
+                    resumeChanges += "distribution, ";
+                }
+                if (jobObservationEntity.OperationId != jobObservationForUpdate.OperationId)
+                {
+                    resumeChanges += "operation, ";
+                }
+                if (jobObservationEntity.SupervisorId != jobObservationForUpdate.SupervisorId)
+                {
+                    resumeChanges += "supervisor, ";
+                }
+                if (jobObservationEntity.OperatorId != jobObservationForUpdate.OperatorId)
+                {
+                    resumeChanges += "operator, ";
+                }
+                if (jobObservationEntity.Type != jobObservationForUpdate.Type)
+                {
+                    resumeChanges += "Type, ";
+                }
+                if (jobObservationEntity.StartDate != jobObservationForUpdate.StartDate)
+                {
+                    resumeChanges += "date Start, ";
+                }
+                if (jobObservationEntity.EndDate != jobObservationForUpdate.EndDate)
+                {
+                    resumeChanges += "date End, ";
+                }
+                if (jobObservationEntity.FinishedDate != jobObservationForUpdate.FinishedDate)
+                {
+                    resumeChanges += "Date Finalized, ";
+                }
+                if (jobObservationEntity.Justification != jobObservationForUpdate.Justification)
+                {
+                    resumeChanges += "Justification , ";
+                }
+                if (jobObservationEntity.Status != jobObservationForUpdate.Status)
+                {
+                    resumeChanges += "Status, ";
+                }
+                if (jobObservationEntity.Option != jobObservationForUpdate.Option)
+                {
+                    resumeChanges += "Option, ";
+                }
+                if (jobObservationEntity.Anomaly != jobObservationForUpdate.Anomaly)
+                {
+                    resumeChanges += "Anomaly, ";
+                }
+                if (jobObservationEntity.Time1HOE != jobObservationForUpdate.Time1HOE)
+                {
+                    resumeChanges += "Timers 1, ";
+                }
+                if (jobObservationEntity.Time2HOE != jobObservationForUpdate.Time2HOE)
+                {
+                    resumeChanges += "Timers 2, ";
+                }
+                if (jobObservationEntity.Models != jobObservationForUpdate.Models)
+                {
+                    resumeChanges += "Models, ";
+                }
+                if (jobObservationEntity.Cicles != jobObservationForUpdate.Cicles)
+                {
+                    resumeChanges += "Cicles, ";
+                }
+                if (jobObservationEntity.SsvCommentary != jobObservationForUpdate.SsvCommentary)
+                {
+                    resumeChanges += "SsvCommentary, ";
+                }
+                if (jobObservationEntity.OperatorCommentary != jobObservationForUpdate.OperatorCommentary)
+                {
+                    resumeChanges += "OperatorCommentary, ";
+                }
+                if (jobObservationEntity.SsvSignature != jobObservationForUpdate.SsvSignature)
+                {
+                    resumeChanges += "SsvSignature, ";
+                }
+                if (jobObservationEntity.OperatorSignature != jobObservationForUpdate.OperatorSignature)
+                {
+                    resumeChanges += "OperatorSignature, ";
+                }
+
+                // Remove the trailing comma and space
+                if (resumeChanges.EndsWith(", "))
+                {
+                    resumeChanges = resumeChanges.Substring(0, resumeChanges.Length - 2);
+                }
+
+                //Actualiza la jobobsevation
+                _mapper.Map(jobObservationForUpdate, jobObservationEntity);
+
+                //añadimos la version anterior a la jobOb actualizada
+                if (HistoryToAdd != null)
+                {
+                    //optenemos la nueva version
+                    var jobtoaddversion = await _supervisorMobilityRepository.GetJobObservationAsync(jobObservationId, true);
+                    HistoryToAdd.DateModification = DateTime.Now;
+                    HistoryToAdd.resumeVersion = resumeChanges;
+                    HistoryToAdd.MadeBy = auser.name;
+                    //añadimos
+                    bool added = await _supervisorMobilityRepository.AddHistoyToJobObservationAsync(HistoryToAdd, jobtoaddversion);
+                    //add to 
+
+                    if (!added)
+                    {
+                        return NotFound("Fail updating history");
+                    }
+
+                }
+            
 
 
             await _supervisorMobilityRepository.SaveChangesAsync();
