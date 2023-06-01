7762cd130c348624a933e43c7ff14422100e5e7c
Marco Antonio Aguayo <MikoGrenore@Outlook.com>
2023-06-01 11:58:44 -0600

    ILU Controller

--- SupervisorMobility.API/Controllers/ILU_Registers_Controller.cs
+++ SupervisorMobility.API/Controllers/ILU_Registers_Controller.cs
@@ -1,22 +1,25 @@
 ﻿using AutoMapper;
+using DocumentFormat.OpenXml.Spreadsheet;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SupervisorMobility.API.DataAccess.Entities;
+using SupervisorMobility.API.DataAccess.Entities.ILU;
 using SupervisorMobility.API.Models.AreaDtos;
+using SupervisorMobility.API.Models.ILURegisterDtos;
 using SupervisorMobility.API.Models.PATDtos;
 using SupervisorMobility.API.Services;
 
 namespace SupervisorMobility.API.Controllers
 {
-        [Route("api/ILU_Registers")]
+        [Route("api/ILU")]
         [ApiController]
 
-    public class ILU_Registers_Controller : ControllerBase
+    public class ILU_RegistersController : ControllerBase
     {
         private readonly IMapper _mapper;
         private readonly ISupervisorMobilityRepository _supervisorMobilityRepository;
 
-        public ILU_Registers_Controller(ISupervisorMobilityRepository supervisorMobilityRepository,
+        public ILU_RegistersController(ISupervisorMobilityRepository supervisorMobilityRepository,
             IMapper mapper)
         {
             _supervisorMobilityRepository = supervisorMobilityRepository ??
@@ -27,38 +30,43 @@ namespace SupervisorMobility.API.Controllers
 
 
 
-        //[HttpPost]
-        //public async Task<ActionResult> AddNewPat(PATFotCreationDto PatForCreate)
-        //{
-        //    var finalPat = _mapper.Map<PAT>(PatForCreate);
-        //    var result = await _supervisorMobilityRepository.AddPat(finalPat);
-            
-        //    if(result > 0){
-        //        return Ok(finalPat);
-        //    }
+        [HttpPost("Register")]
+        public async Task<ActionResult> AddNewPat(ILURegisterForCreationDto ILUToRegister, int userID)
+        {
+            var finalILURegister = _mapper.Map<ILURegister>(ILUToRegister);
 
-        //    return NotFound();
-        //}
+            User MasterUser = await _supervisorMobilityRepository.GetUserAsync(userID);
+          
+            if(MasterUser != null)
+            {
+                var result = await _supervisorMobilityRepository.AddILURegToUser(finalILURegister, MasterUser);
+
+                if (result > 0)
+                {
+                    return Ok(finalILURegister);
+                }
+
+            }
+
+            return NotFound();
+        }
 
 
-        //[HttpGet("{PATid}")]
-        //public async Task<ActionResult<PATDto>> getPatById(
-        //          int PATid, bool includeCollections = false)
-        //{
-        //    if (includeCollections)
-        //    {
-        //        var PatsWhitCollections = await _supervisorMobilityRepository.GetPat(PATid);
-        //        return Ok(_mapper.Map<PATDto>(PatsWhitCollections));
+        [HttpGet("Register/{PATid}")]
+        public async Task<ActionResult<ILURegisterDto>> getPatById(int ILURegisterId, bool includeCollections = false)
+        {
+            if (includeCollections)
+            {
+                var PatsWhitCollections = await _supervisorMobilityRepository.GetILURegister(ILURegisterId);
+                return Ok(_mapper.Map<ILURegisterDto>(PatsWhitCollections));
 
-        //    }
-        //    else
-        //    {
-        //        var Pat = await _supervisorMobilityRepository
-        //                        .GetPat(PATid);
-        //        return Ok(_mapper.Map<PATwithoutNavigations>(Pat));
-
-        //    }
-        //}
+            }
+            else
+            {
+                var Pat = await _supervisorMobilityRepository.GetPat(ILURegisterId);
+                return Ok(_mapper.Map<ILURegisterWithoutNavigationDto>(Pat));
+            }
+        }
 
         //[HttpGet]
         //public async Task<ActionResult<IEnumerable<PATDto>>> getAllPats(bool includeCollections = false)
