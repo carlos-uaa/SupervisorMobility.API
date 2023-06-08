54b2ee7890c97a2b8613a3622ae62006f7b4551b
Marco Antonio Aguayo <MikoGrenore@Outlook.com>
2023-06-08 16:53:48 -0600

    .

--- SupervisorMobility.API/DataAccess/Services/EmailService.cs
+++ SupervisorMobility.API/DataAccess/Services/EmailService.cs
@@ -30,14 +30,17 @@ namespace SupervisorMobility.API.DataAccess.Services
             {
                 try
                 {
+                    //Development
                     client.Connect(_emailConfig.SmtpServer, _emailConfig.Port);
                     client.Authenticate(_emailConfig.UserName, _emailConfig.Password);
 
+
+                    //Production
                     //client.Connect(_emailConfig.SmtpServer, _emailConfig.Port, SecureSocketOptions.None);
                     
+                    
                     client.Send(mailMessage);
-                    client.Disconnect(true);
-                    client.Dispose();
+                   
                 }
                 catch
                 {
