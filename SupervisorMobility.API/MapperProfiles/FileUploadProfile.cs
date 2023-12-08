590129f56cb1af997cf278430acd853c4aa0a351
Marco Aguayo <maguayo@gruposinco.com.mx>
2023-12-07 23:32:49 -0600

    feat

--- SupervisorMobility.API/MapperProfiles/FileUploadProfile.cs
+++ SupervisorMobility.API/MapperProfiles/FileUploadProfile.cs
@@ -6,7 +6,8 @@ namespace SupervisorMobility.API.MapperProfiles
     {
         public FileUploadProfile()
         {
-            CreateMap<DataAccess.Entities.FileUpload, Models.FileUploadDto.FileUploadGeneralDto>();
+            CreateMap<DataAccess.Entities.FileUpload, Models.FileUploadDto.FileUploadGeneralDto>().ReverseMap();
+            CreateMap<Models.FileUploadDto.FileUploadGeneralDto, DataAccess.Entities.FileUpload>().ReverseMap();
             CreateMap<DataAccess.Entities.FileUpload, Models.FileUploadDto.FileUploadForCreationDto>().ReverseMap();
             //CreateMap<DataAccess.Entities.FileUpload, Models.FileUploadDto.AreaForCreationDto>().ReverseMap();
             //CreateMap<DataAccess.Entities.FileUpload, Models.FileUploadDto.AreaForUpdateDto>().ReverseMap();
