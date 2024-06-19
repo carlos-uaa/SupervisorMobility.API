9aed7a0c81ef7544d070051e8dcfeab9ae8dce8b
Pedro Muñoz <pmunoz@gruposinco.com.mx>
2024-06-19 11:39:17 -0600

    feat: create mapper sos hub profile

---
+++ SupervisorMobility.API/MapperProfiles/ISOSMapperProfiles/SOSDataHubProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using SupervisorMobility.API.DataAccess.Entities.SOS;
+using SupervisorMobility.API.Models.SOS.SOSHubDtos;
+
+namespace SupervisorMobility.API.MapperProfiles.ISOSMapperProfiles
+{
+    public class SOSDataHubProfile : Profile
+    {
+        public SOSDataHubProfile()
+        {
+            CreateMap<SOSHub, SOSHubDto>().ReverseMap();
+            CreateMap<SOSHub, SOSHubForCreateDto>().ReverseMap();
+            CreateMap<SOSHub, SOSHubForUpdateDto>().ReverseMap();
+        }
+    }
+}
