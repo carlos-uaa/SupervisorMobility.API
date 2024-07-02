139a3edc6a45de4c14a3aa25bcce636a25ad39cb
Marco Antonio Aguayo <MikoGrenore@Outlook.com>
2024-07-02 11:30:41 -0600

    Feat:
    Add Entitys: Analysis, Section

--- SupervisorMobility.API/DataAccess/Entities/SOS/AnalysisBkup.cs
+++ SupervisorMobility.API/DataAccess/Entities/SOS/AnalysisBkup.cs
@@ -7,7 +7,9 @@ namespace SupervisorMobility.API.DataAccess.Entities.SOS
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int AnalysisBkupId { get; set; }
+
         public string Text { get; set; }
         public bool? IsActive { get; set; }
+
     }
 }
