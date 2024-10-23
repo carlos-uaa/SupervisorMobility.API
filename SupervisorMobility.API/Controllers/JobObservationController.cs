d13e97fe9b880c4861940b223a0bdac5149458de
Marco Aguayo <maguayo@gruposinco.com.mx>
2024-10-23 15:30:20 -0600

    Feat:
    Update logic yo update next job obsevation

--- SupervisorMobility.API/Controllers/JobObservationController.cs
+++ SupervisorMobility.API/Controllers/JobObservationController.cs
@@ -332,15 +332,12 @@ namespace SupervisorMobility.API.Controllers
 
                         if (NextYearJob != null)
                         {
-                            //existe en caso de ser una fecha mas reciente actualizar
-
+                            
                             Distribution distribution = await _supervisorMobilityRepository.GetDistributionOnlyIdAsync((int)jobObservationEntity.DistributionId, false);
 
                             DateTime FechaActual = jobObservationForUpdate.FinishedDate.Value.AddYears(1);
 
-                            if(FechaActual < NextYearJob.StartDate)
-                            {
-
+                            
                                 NotificationToCreateDto NotifyUpdateNextYear = new NotificationToCreateDto();
                                 NotifyUpdateNextYear.MadeBy = auser;
                                 NotifyUpdateNextYear.UserId = jobObservationForUpdate.SupervisorId;
@@ -358,7 +355,7 @@ namespace SupervisorMobility.API.Controllers
                                 NextYearJob.StartDate = FechaActual;
 
                                 await _supervisorMobilityRepository.SaveChangesAsync();
-                            }
+                            
 
                         }
                         else
