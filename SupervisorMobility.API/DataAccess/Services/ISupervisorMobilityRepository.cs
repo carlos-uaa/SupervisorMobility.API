3127070be4aef3aa92c11cc65164971ece36991b
Marco Aguayo <maguayo@gruposinco.com.mx>
2024-11-04 20:07:31 +0000

    Updated ISupervisorMobilityRepository.cs

--- SupervisorMobility.API/DataAccess/Services/ISupervisorMobilityRepository.cs
+++ SupervisorMobility.API/DataAccess/Services/ISupervisorMobilityRepository.cs
@@ -227,7 +227,7 @@ namespace SupervisorMobility.API.Services
             bool includeHistory = false, bool includeCkAnswers = false, int idPlant = 0, int idArea = 0, bool ForSosProgram = false, int year = 0, int month = 0,
             int SOSAnualId = 0, int idUser = 0);
         Task<JOPaginationDto> GetJobObservationsByFiltersAsync(DateTime startDate, DateTime endDate, int jobObsId, int plantId, int areaId, int distributionId, int operationId, int supervisorId, int status, int userId, int typeId, string searchString, int page = 1, int entries = 10, int? sortO = 2, string? sortL = "");
-        Task<JobObservation?> GetJobObservationAsync(int jobObservationId, bool includeTree = false, bool includePeople = false, bool includeLup = false, bool includeHistory = false, bool includeCkAnswers = false);
+        Task<JobObservation?> GetJobObservationAsync(int jobObservationId, bool includeTree = false, bool includePeople = false, bool includeLup = false, bool includeHistory = false, bool includeCkAnswers = false, bool includeOperations = false);
         Task<JobObservation?> FindNextYearJobObservation(int plantId, int areaId, int DistributionId, int operationId, int supervisorId, int year);
         Task<IEnumerable<JobObservation>> GetAllNextYearJobsObservations(int plantId, int areaId, int year);
         Task<int> AddJobObservation(JobObservation jobObservation);
