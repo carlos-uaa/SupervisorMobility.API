4d250ba843de030e6437dc63a89edae3cb03efd9
Vicente <vmedina@gruposinco.com.mx>
2024-12-03 10:43:48 -0600

    feat: added pat user roles

---
+++ SupervisorMobility.API/DataAccess/Entities/PatUserRole.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SupervisorMobility.API.DataAccess.Entities
+{
+    public enum OperatorRole
+    {
+        SV,
+        Lider,
+        CA, // C/A
+        NI
+    }
+
+    public class PatUserRole
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int PATId { get; set; }
+        public int UserId { get; set; }
+        public OperatorRole? Role { get; set; }
+        public bool isActive { get; set; }
+
+    }
+}
