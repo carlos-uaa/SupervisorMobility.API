3d3e045f42f4a31008cfd6e161678c41e77f7c0f
Marco Aguayo <maguayo@gruposinco.com.mx>
2024-12-20 16:38:00 -0600

    Feat:
    Save changes

--- SupervisorMobility.API/Models/Users/UsersForUpdateDto.cs
+++ SupervisorMobility.API/Models/Users/UsersForUpdateDto.cs
@@ -30,7 +30,7 @@ namespace SupervisorMobility.API.Models.Users
         public int? GroupId { get; set; }
         public int? DistributionId { get; set; }
         public ICollection<UsersWithoutNavigationWithoutPeopleDetails>? Subordinates { get; set; }
-        public ICollection<ILURegisterWithoutNavigationDto>? ILURegisers { get; set; }
+        //public ICollection<ILURegisterWithoutNavigationDto>? ILURegisers { get; set; }
 
         public ICollection<AreaDtos.AreaWithoutNavigationPropertiesDto>? Areas { get; set; }
 
