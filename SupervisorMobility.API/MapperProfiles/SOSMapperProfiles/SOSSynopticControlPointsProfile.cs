b7b8ca88e004c7e8df0ed614258a332fbf107265
BRAYAN CAPETILLO <bcapetillo@gruposinco.com.mx>
2025-08-13 11:45:23 -0600

    feat(mapper): add SOSSynopticControlPointsProfile map

---
+++ SupervisorMobility.API/MapperProfiles/SOSMapperProfiles/SOSSynopticControlPointsProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using SupervisorMobility.API.DataAccess.Entities.SOS;
+using SupervisorMobility.API.Models.SOS.SOSSynopticTableofControlPointsDtos;
+
+namespace SupervisorMobility.API.MapperProfiles.SOSMapperProfiles
+{
+    public class SOSSynopticControlPointsProfile : Profile
+    {
+        public SOSSynopticControlPointsProfile()
+        {
+            CreateMap<SOSSynopticTableofControlPoints, SOSSynopticControlPointsDto>().ReverseMap();
+            CreateMap<SOSSynopticTableofControlPoints, SOSSynopticTableofControlPointsForCreateDto>().ReverseMap();
+        }
+    }
+}
