2ec72078207a294e5449dd830c127368d7babc51
BRAYAN CAPETILLO <bcapetillo@gruposinco.com.mx>
2025-09-04 23:28:41 -0600

    chore(migrations): add all pending migrations

---
+++ SupervisorMobility.API/Migrations/20250827220001_migrationTest.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SupervisorMobility.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class migrationTest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsMachineOperation",
+                table: "Analyses",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Notifications",
+                keyColumn: "NotificationID",
+                keyValue: 2,
+                column: "EntryDate",
+                value: new DateTime(2025, 8, 27, 16, 0, 0, 149, DateTimeKind.Local).AddTicks(1360));
+
+            migrationBuilder.UpdateData(
+                table: "Notifications",
+                keyColumn: "NotificationID",
+                keyValue: 3,
+                column: "EntryDate",
+                value: new DateTime(2025, 8, 27, 16, 0, 0, 149, DateTimeKind.Local).AddTicks(1362));
+
+            migrationBuilder.UpdateData(
+                table: "Notifications",
+                keyColumn: "NotificationID",
+                keyValue: 4,
+                column: "EntryDate",
+                value: new DateTime(2025, 8, 27, 16, 0, 0, 149, DateTimeKind.Local).AddTicks(1364));
+
+            migrationBuilder.UpdateData(
+                table: "Notifications",
+                keyColumn: "NotificationID",
+                keyValue: 5,
+                column: "EntryDate",
+                value: new DateTime(2025, 8, 27, 16, 0, 0, 149, DateTimeKind.Local).AddTicks(1366));
+
+            migrationBuilder.UpdateData(
+                table: "Notifications",
+                keyColumn: "NotificationID",
+                keyValue: 6,
+                column: "EntryDate",
+                value: new DateTime(2025, 8, 27, 16, 0, 0, 149, DateTimeKind.Local).AddTicks(1367));
+
+            migrationBuilder.UpdateData(
+                table: "Notifications",
+                keyColumn: "NotificationID",
+                keyValue: 7,
+                column: "EntryDate",
+                value: new DateTime(2025, 8, 27, 16, 0, 0, 149, DateTimeKind.Local).AddTicks(1369));
+
+            migrationBuilder.UpdateData(
+                table: "Notifications",
+                keyColumn: "NotificationID",
+                keyValue: 8,
+                column: "EntryDate",
+                value: new DateTime(2025, 8, 27, 16, 0, 0, 149, DateTimeKind.Local).AddTicks(1371));
+
+            migrationBuilder.UpdateData(
+                table: "Notifications",
+                keyColumn: "NotificationID",
+                keyValue: 9,
+                column: "EntryDate",
+                value: new DateTime(2025, 8, 27, 16, 0, 0, 149, DateTimeKind.Local).AddTicks(1373));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 8, 27, 16, 0, 0, 149, DateTimeKind.Local).AddTicks(1151));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2025, 8, 27, 16, 0, 0, 149, DateTimeKind.Local).AddTicks(1164));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2025, 8, 27, 16, 0, 0, 149, DateTimeKind.Local).AddTicks(1166));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2025, 8, 27, 16, 0, 0, 149, DateTimeKind.Local).AddTicks(1168));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 5,
+                column: "CreatedDate",
+                value: new DateTime(2025, 8, 27, 16, 0, 0, 149, DateTimeKind.Local).AddTicks(1174));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 6,
+                column: "CreatedDate",
+                value: new DateTime(2025, 8, 27, 16, 0, 0, 149, DateTimeKind.Local).AddTicks(1176));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 7,
+                column: "CreatedDate",
+                value: new DateTime(2025, 8, 27, 16, 0, 0, 149, DateTimeKind.Local).AddTicks(1179));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 8,
+                column: "CreatedDate",
+                value: new DateTime(2025, 8, 27, 16, 0, 0, 149, DateTimeKind.Local).AddTicks(1188));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsMachineOperation",
+                table: "Analyses",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.UpdateData(
+                table: "Notifications",
+                keyColumn: "NotificationID",
+                keyValue: 2,
+                column: "EntryDate",
+                value: new DateTime(2025, 8, 27, 12, 7, 30, 252, DateTimeKind.Local).AddTicks(8464));
+
+            migrationBuilder.UpdateData(
+                table: "Notifications",
+                keyColumn: "NotificationID",
+                keyValue: 3,
+                column: "EntryDate",
+                value: new DateTime(2025, 8, 27, 12, 7, 30, 252, DateTimeKind.Local).AddTicks(8467));
+
+            migrationBuilder.UpdateData(
+                table: "Notifications",
+                keyColumn: "NotificationID",
+                keyValue: 4,
+                column: "EntryDate",
+                value: new DateTime(2025, 8, 27, 12, 7, 30, 252, DateTimeKind.Local).AddTicks(8469));
+
+            migrationBuilder.UpdateData(
+                table: "Notifications",
+                keyColumn: "NotificationID",
+                keyValue: 5,
+                column: "EntryDate",
+                value: new DateTime(2025, 8, 27, 12, 7, 30, 252, DateTimeKind.Local).AddTicks(8470));
+
+            migrationBuilder.UpdateData(
+                table: "Notifications",
+                keyColumn: "NotificationID",
+                keyValue: 6,
+                column: "EntryDate",
+                value: new DateTime(2025, 8, 27, 12, 7, 30, 252, DateTimeKind.Local).AddTicks(8472));
+
+            migrationBuilder.UpdateData(
+                table: "Notifications",
+                keyColumn: "NotificationID",
+                keyValue: 7,
+                column: "EntryDate",
+                value: new DateTime(2025, 8, 27, 12, 7, 30, 252, DateTimeKind.Local).AddTicks(8474));
+
+            migrationBuilder.UpdateData(
+                table: "Notifications",
+                keyColumn: "NotificationID",
+                keyValue: 8,
+                column: "EntryDate",
+                value: new DateTime(2025, 8, 27, 12, 7, 30, 252, DateTimeKind.Local).AddTicks(8476));
+
+            migrationBuilder.UpdateData(
+                table: "Notifications",
+                keyColumn: "NotificationID",
+                keyValue: 9,
+                column: "EntryDate",
+                value: new DateTime(2025, 8, 27, 12, 7, 30, 252, DateTimeKind.Local).AddTicks(8477));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 8, 27, 12, 7, 30, 252, DateTimeKind.Local).AddTicks(8256));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2025, 8, 27, 12, 7, 30, 252, DateTimeKind.Local).AddTicks(8278));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2025, 8, 27, 12, 7, 30, 252, DateTimeKind.Local).AddTicks(8280));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2025, 8, 27, 12, 7, 30, 252, DateTimeKind.Local).AddTicks(8281));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 5,
+                column: "CreatedDate",
+                value: new DateTime(2025, 8, 27, 12, 7, 30, 252, DateTimeKind.Local).AddTicks(8284));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 6,
+                column: "CreatedDate",
+                value: new DateTime(2025, 8, 27, 12, 7, 30, 252, DateTimeKind.Local).AddTicks(8292));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 7,
+                column: "CreatedDate",
+                value: new DateTime(2025, 8, 27, 12, 7, 30, 252, DateTimeKind.Local).AddTicks(8304));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 8,
+                column: "CreatedDate",
+                value: new DateTime(2025, 8, 27, 12, 7, 30, 252, DateTimeKind.Local).AddTicks(8306));
+        }
+    }
+}
---
+++ SupervisorMobility.API/Migrations/20250903065836_UpdateTrainingTimeInSOSHub.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SupervisorMobility.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTrainingTimeInSOSHub : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                -- Primero, reemplazar TrainingTime con solo el número si contiene 'cycle'
+                UPDATE SOSHubs
+                SET TrainingTime = 
+                    CASE 
+                        WHEN TrainingTime LIKE '%cycle%' THEN SUBSTRING(TrainingTime, 1, CHARINDEX(' ', TrainingTime)-1)
+                        ELSE TrainingTime
+                    END
+            ");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TrainingTime",
+                table: "SOSHubs",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true
+            );
+
+            migrationBuilder.UpdateData(
+                table: "Notifications",
+                keyColumn: "NotificationID",
+                keyValue: 2,
+                column: "EntryDate",
+                value: new DateTime(2025, 9, 3, 0, 58, 35, 719, DateTimeKind.Local).AddTicks(1863));
+
+            migrationBuilder.UpdateData(
+                table: "Notifications",
+                keyColumn: "NotificationID",
+                keyValue: 3,
+                column: "EntryDate",
+                value: new DateTime(2025, 9, 3, 0, 58, 35, 719, DateTimeKind.Local).AddTicks(1865));
+
+            migrationBuilder.UpdateData(
+                table: "Notifications",
+                keyColumn: "NotificationID",
+                keyValue: 4,
+                column: "EntryDate",
+                value: new DateTime(2025, 9, 3, 0, 58, 35, 719, DateTimeKind.Local).AddTicks(1867));
+
+            migrationBuilder.UpdateData(
+                table: "Notifications",
+                keyColumn: "NotificationID",
+                keyValue: 5,
+                column: "EntryDate",
+                value: new DateTime(2025, 9, 3, 0, 58, 35, 719, DateTimeKind.Local).AddTicks(1869));
+
+            migrationBuilder.UpdateData(
+                table: "Notifications",
+                keyColumn: "NotificationID",
+                keyValue: 6,
+                column: "EntryDate",
+                value: new DateTime(2025, 9, 3, 0, 58, 35, 719, DateTimeKind.Local).AddTicks(1871));
+
+            migrationBuilder.UpdateData(
+                table: "Notifications",
+                keyColumn: "NotificationID",
+                keyValue: 7,
+                column: "EntryDate",
+                value: new DateTime(2025, 9, 3, 0, 58, 35, 719, DateTimeKind.Local).AddTicks(1872));
+
+            migrationBuilder.UpdateData(
+                table: "Notifications",
+                keyColumn: "NotificationID",
+                keyValue: 8,
+                column: "EntryDate",
+                value: new DateTime(2025, 9, 3, 0, 58, 35, 719, DateTimeKind.Local).AddTicks(1874));
+
+            migrationBuilder.UpdateData(
+                table: "Notifications",
+                keyColumn: "NotificationID",
+                keyValue: 9,
+                column: "EntryDate",
+                value: new DateTime(2025, 9, 3, 0, 58, 35, 719, DateTimeKind.Local).AddTicks(1876));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 9, 3, 0, 58, 35, 719, DateTimeKind.Local).AddTicks(1658));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2025, 9, 3, 0, 58, 35, 719, DateTimeKind.Local).AddTicks(1672));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2025, 9, 3, 0, 58, 35, 719, DateTimeKind.Local).AddTicks(1674));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2025, 9, 3, 0, 58, 35, 719, DateTimeKind.Local).AddTicks(1682));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 5,
+                column: "CreatedDate",
+                value: new DateTime(2025, 9, 3, 0, 58, 35, 719, DateTimeKind.Local).AddTicks(1697));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 6,
+                column: "CreatedDate",
+                value: new DateTime(2025, 9, 3, 0, 58, 35, 719, DateTimeKind.Local).AddTicks(1700));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 7,
+                column: "CreatedDate",
+                value: new DateTime(2025, 9, 3, 0, 58, 35, 719, DateTimeKind.Local).AddTicks(1702));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 8,
+                column: "CreatedDate",
+                value: new DateTime(2025, 9, 3, 0, 58, 35, 719, DateTimeKind.Local).AddTicks(1705));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SOSAnalysisSOSSynopticTableofControlPoints");
+
+            migrationBuilder.DropTable(
+                name: "SOSSequenceSOSSynopticTableofControlPoints");
+
+            migrationBuilder.UpdateData(
+                table: "Notifications",
+                keyColumn: "NotificationID",
+                keyValue: 2,
+                column: "EntryDate",
+                value: new DateTime(2025, 9, 3, 0, 52, 57, 735, DateTimeKind.Local).AddTicks(2477));
+
+            migrationBuilder.UpdateData(
+                table: "Notifications",
+                keyColumn: "NotificationID",
+                keyValue: 3,
+                column: "EntryDate",
+                value: new DateTime(2025, 9, 3, 0, 52, 57, 735, DateTimeKind.Local).AddTicks(2480));
+
+            migrationBuilder.UpdateData(
+                table: "Notifications",
+                keyColumn: "NotificationID",
+                keyValue: 4,
+                column: "EntryDate",
+                value: new DateTime(2025, 9, 3, 0, 52, 57, 735, DateTimeKind.Local).AddTicks(2481));
+
+            migrationBuilder.UpdateData(
+                table: "Notifications",
+                keyColumn: "NotificationID",
+                keyValue: 5,
+                column: "EntryDate",
+                value: new DateTime(2025, 9, 3, 0, 52, 57, 735, DateTimeKind.Local).AddTicks(2483));
+
+            migrationBuilder.UpdateData(
+                table: "Notifications",
+                keyColumn: "NotificationID",
+                keyValue: 6,
+                column: "EntryDate",
+                value: new DateTime(2025, 9, 3, 0, 52, 57, 735, DateTimeKind.Local).AddTicks(2490));
+
+            migrationBuilder.UpdateData(
+                table: "Notifications",
+                keyColumn: "NotificationID",
+                keyValue: 7,
+                column: "EntryDate",
+                value: new DateTime(2025, 9, 3, 0, 52, 57, 735, DateTimeKind.Local).AddTicks(2492));
+
+            migrationBuilder.UpdateData(
+                table: "Notifications",
+                keyColumn: "NotificationID",
+                keyValue: 8,
+                column: "EntryDate",
+                value: new DateTime(2025, 9, 3, 0, 52, 57, 735, DateTimeKind.Local).AddTicks(2494));
+
+            migrationBuilder.UpdateData(
+                table: "Notifications",
+                keyColumn: "NotificationID",
+                keyValue: 9,
+                column: "EntryDate",
+                value: new DateTime(2025, 9, 3, 0, 52, 57, 735, DateTimeKind.Local).AddTicks(2495));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 9, 3, 0, 52, 57, 735, DateTimeKind.Local).AddTicks(2282));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2025, 9, 3, 0, 52, 57, 735, DateTimeKind.Local).AddTicks(2306));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2025, 9, 3, 0, 52, 57, 735, DateTimeKind.Local).AddTicks(2308));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2025, 9, 3, 0, 52, 57, 735, DateTimeKind.Local).AddTicks(2309));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 5,
+                column: "CreatedDate",
+                value: new DateTime(2025, 9, 3, 0, 52, 57, 735, DateTimeKind.Local).AddTicks(2313));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 6,
+                column: "CreatedDate",
+                value: new DateTime(2025, 9, 3, 0, 52, 57, 735, DateTimeKind.Local).AddTicks(2316));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 7,
+                column: "CreatedDate",
+                value: new DateTime(2025, 9, 3, 0, 52, 57, 735, DateTimeKind.Local).AddTicks(2318));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 8,
+                column: "CreatedDate",
+                value: new DateTime(2025, 9, 3, 0, 52, 57, 735, DateTimeKind.Local).AddTicks(2321));
+
+
+            migrationBuilder.AddColumn<string>(
+                name: "TrainingTimeTemp",
+                table: "SOSHubs",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.Sql(@"
+                UPDATE SOSHubs
+                SET TrainingTimeTemp = 
+                    CASE 
+                        WHEN TrainingTime IS NOT NULL THEN CAST(TrainingTime AS nvarchar) + ' cycles'
+                        ELSE '0 cycles'
+                    END
+            ");
+
+            migrationBuilder.DropColumn(
+                name: "TrainingTime",
+                table: "SOSHubs");
+
+            migrationBuilder.RenameColumn(
+                name: "TrainingTimeTemp",
+                table: "SOSHubs",
+                newName: "TrainingTime");
+        }
+    }
+}
