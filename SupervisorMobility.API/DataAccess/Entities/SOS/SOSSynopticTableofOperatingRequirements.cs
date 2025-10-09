54e9cf1806349a324fc600ae3594a352018cfe39
BRAYAN CAPETILLO <bcapetillo@gruposinco.com.mx>
2025-10-09 16:22:16 -0600

    ✨ feat(entity): add InsuranceFeatures property to SOSSynopticTableofOperatingRequirements entity

--- SupervisorMobility.API/DataAccess/Entities/SOS/SOSSynopticTableofOperatingRequirements.cs
+++ SupervisorMobility.API/DataAccess/Entities/SOS/SOSSynopticTableofOperatingRequirements.cs
@@ -45,5 +45,6 @@ namespace SupervisorMobility.API.DataAccess.Entities.SOS
         public ICollection<SOSSTROKnowledgeHub>? SOSSTROKnowledge { get; set; } = new List<SOSSTROKnowledgeHub>();
         public ICollection<SOSSTROSkillHub>? SOSSTROSkill { get; set; } = new List<SOSSTROSkillHub>();
         public ICollection<EstablishedConditions>? EstablishedConditions { get; set; } = new List<EstablishedConditions>();
+        public ICollection<InsuranceFeatures>? InsuranceFeatures { get; set; } = new List<InsuranceFeatures>();
     }
 }
