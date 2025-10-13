f0d2031537813e0b1c598fa6324eca595e00f0f8
BRAYAN CAPETILLO <bcapetillo@gruposinco.com.mx>
2025-10-13 12:45:49 -0600

    ✨ feat(dto): add OperationMachine property to SOSSynopticRequirementsDto dto

--- SupervisorMobility.API/Models/SOS/SOSSynopticTableofOperatingRequirementsDtos/SOSSynopticRequirementsDto.cs
+++ SupervisorMobility.API/Models/SOS/SOSSynopticTableofOperatingRequirementsDtos/SOSSynopticRequirementsDto.cs
@@ -57,5 +57,6 @@ namespace SupervisorMobility.API.Models.SOS.SOSSynopticTableofOperatingRequireme
         public IEnumerable<SOSSTROSkillHub>? SOSSTROSkill { get; set; } = new List<SOSSTROSkillHub>();
         public ICollection<EstablishedConditions>? EstablishedConditions { get; set; } = new List<EstablishedConditions>();
         public ICollection<InsuranceFeatures>? InsuranceFeatures { get; set; } = new List<InsuranceFeatures>();
+        public ICollection<OperationMachine>? OperationMachine { get; set; } = new List<OperationMachine>();
     }
 }
