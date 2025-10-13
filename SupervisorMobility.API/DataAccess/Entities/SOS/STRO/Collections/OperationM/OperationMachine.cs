34823f5edefce57b33086f7b3028d96336413519
BRAYAN CAPETILLO <bcapetillo@gruposinco.com.mx>
2025-10-13 12:41:56 -0600

    ✨ feat(entity): add OperationMachine entity

---
+++ SupervisorMobility.API/DataAccess/Entities/SOS/STRO/Collections/OperationM/OperationMachine.cs
@@ -0,0 +1,19 @@
+﻿
+// - Core .NET imports
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+// - Custom project imports
+using SupervisorMobility.API.DataAccess.Entities.SOS.STRO.Collections;
+
+namespace SupervisorMobility.API.DataAccess.Entities.SOS.STRO
+{
+    public class OperationMachine
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Operation { get; set; } = string.Empty;
+        public int SOSSynopticTableofOperatingRequirementsId { get; set; }
+        public int SectionId { get; set; }
+    }
+}
