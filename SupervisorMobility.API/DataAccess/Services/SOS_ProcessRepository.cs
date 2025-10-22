f28d77a49d50750b467d6358db51fdfc004243b8
enmanuel diaz almeida <ediaz@gruposinco.com.mx>
2025-10-22 13:30:00 -0600

    Fix: 🐛 :- : Fourth Cycle: Histories 7520 and 7535 (Set  filter by User's Areas) validamos que el usuarioid sea diferente de 0 y mayor que 0, para evitar llamadas innecesarias

--- SupervisorMobility.API/DataAccess/Services/SOS_ProcessRepository.cs
+++ SupervisorMobility.API/DataAccess/Services/SOS_ProcessRepository.cs
@@ -311,31 +311,39 @@ namespace SupervisorMobility.API.DataAccess.Services
             IQueryable<SOSHub> query;
 
             //si tiene mas de una area asignada hacemos el filtrado por las areas asignadas
-            var areaIds = await _context.Users.AsNoTracking().Where(u => u.UserId == userId).Select(u => u.Areas.Select(a => a.AreaId).ToList()).FirstOrDefaultAsync();
-
-            if (areaIds != null && areaIds.Count>0)
+            if (userId != 0 && userId>0)
             {
+                var areaIds = await _context.Users.AsNoTracking().Where(u => u.UserId == userId).Select(u => u.Areas.Select(a => a.AreaId).ToList()).FirstOrDefaultAsync();
 
-                query = _context.SOSHubs.AsNoTracking().Where(h => h.IsActive == true && areaIds.Contains((int)h.AreaId));
-            }
-            else
-            {
-                //buscamos si el usuario tiene solo una area asignada 
-                var areaId = await _context.Users.AsNoTracking().Where(u => u.UserId == userId).Select(a => a.AreaId).FirstOrDefaultAsync();
-
-                //si solo tiene una sola area hacemos el filtrado sencillo
-                if (areaId != null && areaId != 0)
+                if (areaIds != null && areaIds.Count > 0)
                 {
-                    query = _context.SOSHubs.AsNoTracking().Where(h => h.IsActive == true && h.AreaId == areaId);
+
+                    query = _context.SOSHubs.AsNoTracking().Where(h => h.IsActive == true && h.AreaId!=null && areaIds.Contains((int)h.AreaId));
                 }
                 else
                 {
-                    query = _context.SOSHubs.AsNoTracking().Where(h => h.IsActive == true);
+                    //buscamos si el usuario tiene solo una area asignada 
+                    var areaId = await _context.Users.AsNoTracking().Where(u => u.UserId == userId).Select(a => a.AreaId).FirstOrDefaultAsync();
+
+                    //si solo tiene una sola area hacemos el filtrado sencillo
+                    if (areaId != null && areaId != 0)
+                    {
+                        query = _context.SOSHubs.AsNoTracking().Where(h => h.IsActive == true && h.AreaId == areaId);
+                    }
+                    else
+                    {
+                        query = _context.SOSHubs.AsNoTracking().Where(h => h.IsActive == true);
+                    }
                 }
             }
-           
-           
-           
+            else
+            {
+                query = _context.SOSHubs.AsNoTracking().Where(h => h.IsActive == true);
+            }
+
+
+
+
 
 
             if (includeAnalysesBkup)
