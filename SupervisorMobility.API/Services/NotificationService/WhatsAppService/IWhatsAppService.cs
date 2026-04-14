595451ae19a6d646a4c005a500ec5a62b9cb3b67
gmtzdev <gerardomtzisc@gmail.com>
2026-04-14 17:16:29 -0600

    Add WhatsAppService implementation for sending templates

---
+++ SupervisorMobility.API/Services/NotificationService/WhatsAppService/IWhatsAppService.cs
@@ -0,0 +1,7 @@
+﻿namespace SupervisorMobility.API.Services.WhatsAppService
+{
+    public interface IWhatsAppService
+    {
+        Task<bool> SendWhatsAppTemplateAsync(string recipientPhoneNumber, string whatsAppTemplate);
+    }
+}
