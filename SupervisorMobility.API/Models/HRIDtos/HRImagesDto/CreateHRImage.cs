83abd6bfa580e52a4be79514afe11772a8f312b3
gmtzdev <gerardomtzisc@gmail.com>
2026-04-17 11:43:06 -0600

    Feat : : Change HriId property to non-nullable in CreateHRImageDto

--- SupervisorMobility.API/Models/HRIDtos/HRImagesDto/CreateHRImage.cs
+++ SupervisorMobility.API/Models/HRIDtos/HRImagesDto/CreateHRImage.cs
@@ -4,7 +4,7 @@ namespace SupervisorMobility.API.Models.HRIDtos.HRImagesDto
 {
     public class CreateHRImageDto
     {
-        public int? HriId { get; set; }
+        public int HriId { get; set; }
         public string ImageUrl { get; set; }
         public string ImageType { get; set; }
     }
