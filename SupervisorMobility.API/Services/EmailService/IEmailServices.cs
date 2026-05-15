b550f07b7d6bbe7e56073380efec91e562c1d671
gmtzdev <gerardomtzisc@gmail.com>
2026-05-15 14:04:13 -0600

    Refactor: Clean up unused using directives in IEmailServices interface

--- SupervisorMobility.API/Services/EmailService/IEmailServices.cs
+++ SupervisorMobility.API/Services/EmailService/IEmailServices.cs
@@ -1,14 +1,9 @@
-﻿// using SupervisorMobility.API.DTOs;
-using SupervisorMobility.API.Models;
-using SupervisorMobility.API.Models.Email;
-using MailKit.Net.Smtp;
-using MimeKit;
+﻿using SupervisorMobility.API.Models.Email;
 
 namespace SupervisorMobility.API.Services.EmailService
 {
     public interface IEmailServices
     {
         public Task<bool> SendEmailAsync(EmailQueue queued);
-        
     }
 }
