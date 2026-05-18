f3eca66414ec6849ef0239e8f9ee339d7a309174
enmanuel diaz almeida <ediaz@gruposinco.com.mx>
2026-05-18 11:20:10 -0600

    Fix : : Add Moq Package

---
+++ Tests/HRITests/HRItemsTest/GetInMemoryDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SupervisorMobility.API.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests.HRITests.HRItemsTest
+{
+    public class GetInMemoryDBContext
+    {
+        public SupervisorMobilityContext GetInMemoryDbContext()
+        {
+            var options = new DbContextOptionsBuilder<SupervisorMobilityContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+            var context = new SupervisorMobilityContext(options);
+            context.Database.EnsureCreated();
+            return context;
+        }
+    }
+}
--- Tests/HRITests/HRItemsTest/HRIItemsTest.cs
+++ Tests/HRITests/HRItemsTest/HRIItemsTest.cs
@@ -1,10 +1,19 @@
-﻿using System;
+﻿using Moq;
+using SupervisorMobility.API.DataAccess.Services.HRIServices;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Tests.HRITests.HRItemsTest
 {
-    internal class HRIItemsTest
+    
+    public class HRIItemsTest
     {
+        [Test]
+        public async Task CreateHRIItemAsync()
+        {
+            // arrange
+            var mockService = new Mock<IHRIItemsService>();
+        }
     }
 }
